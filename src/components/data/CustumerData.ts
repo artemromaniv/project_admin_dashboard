b1d611b2085f0bca039cc4bce830fe193c72e331
Artem <artem_romaniv@protonmail.com>
2022-08-17 02:18:09 -0700

    add more pages to customers table

--- src/components/data/CustumerData.ts
+++ src/components/data/CustumerData.ts
@@ -12,26 +12,208 @@ export const data:DataType[] = [
     {
         key:'1',
         name:'Tajima Haru',
-        email:'tajimaharu@gmail.com',
+        email:'tajima.haru@example.com',
         location:'Japan',
         lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
     },
     {
         key:'2',
-        name:'Tajima Haru',
-        email:'tajimaharu@gmail.com',
-        location:'Japan',
-        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+        name:'Jacob Byrd',
+        email:'jacob.byrd@example.com',
+        location:'UK',
+        lastActive:dayjs('2022-04-09').format('DD.MM.YYYY')
     },
     {
         key:'3',
+        name:'Justin Fowler',
+        email:'justin.fowler@example.com',
+        location:'USA',
+        lastActive:dayjs('2022-03-08').format('DD.MM.YYYY')
+    },
+    {
+        key:'4',
+        name:'Margie Gordon',
+        email:'margie.gordon@example.com',
+        location:'UK',
+        lastActive:dayjs('2022-02-22').format('DD.MM.YYYY')
+    },
+    {
+        key:'5',
+        name:'Alex Ortiz',
+        email:'alex.ortiz@example.com',
+        location:'Germany',
+        lastActive:dayjs('2022-01-06').format('DD.MM.YYYY')
+    },
+    {
+        key:'6',
+        name:'Frederick Caldwell',
+        email:'frederick.caldwell@example.com',
+        location:'UK',
+        lastActive:dayjs('2022-05-12').format('DD.MM.YYYY')
+    },
+    {
+        key:'7',
+        name:'Gregory Howard',
+        email:'gregory.howard@example.com',
+        location:'USA',
+        lastActive:dayjs('2022-01-06').format('DD.MM.YYYY')
+    },
+    {
+        key:'8',
+        name:'Hamada Takuma',
+        email:'hamada.takuma@example.com',
+        location:'Japan',
+        lastActive:dayjs('2022-03-18').format('DD.MM.YYYY')
+    },
+    {
+        key:'9',
+        name:'Yasuhiro Fujimoto',
+        email:'yasuhiro.fujimoto@example.com',
+        location:'Japan',
+        lastActive:dayjs('2022-02-01').format('DD.MM.YYYY')
+    },
+    {
+        key:'10',
+        name:'Travis Gordon',
+        email:'travis.gordon@example.com',
+        location:'USA',
+        lastActive:dayjs('2022-06-10').format('DD.MM.YYYY')
+    },
+    {
+        key:'11',
         name:'Tajima Haru',
         email:'tajimaharu@gmail.com',
         location:'Japan',
         lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
     },
     {
-        key:'4',
+        key:'12',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'13',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'14',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'15',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'16',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'17',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'18',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'19',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'20',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'21',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'22',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'23',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'24',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'25',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'26',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'27',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'28',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'29',
+        name:'Tajima Haru',
+        email:'tajimaharu@gmail.com',
+        location:'Japan',
+        lastActive:dayjs('2022-05-25').format('DD.MM.YYYY')
+    },
+    {
+        key:'30',
         name:'Tajima Haru',
         email:'tajimaharu@gmail.com',
         location:'Japan',
