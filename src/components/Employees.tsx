aa3f4093e9ae20e37a00a2a920c6bd9c9eaedcf3
Artem <artem_romaniv@protonmail.com>
2022-08-19 00:07:11 -0700

    add employee table

--- src/components/Employees.tsx
+++ src/components/Employees.tsx
@@ -1,10 +1,11 @@
-
-
+import {Table } from "antd"
+import { Content } from "antd/lib/layout/layout"
+import {data,columns} from './data/employeeData'
 const Employees = () => {
   return (
-    <div>
-      
-    </div>
+    <Content className="table-container">
+      <Table dataSource={data} columns = {columns}/>
+    </Content>
   )
 }
 
---
+++ src/components/data/employeeData.ts
@@ -0,0 +1,50 @@
+import type {ColumnsType} from 'antd/es/table'
+
+interface DataType {
+    key:number,
+    employee:string,
+    position:string,
+    email:string,
+    status: string,
+    salary:number
+}
+
+export const data:DataType[] = [
+    {
+        key:1,
+        employee:'Ariene Fox',
+        position:'Lead UX Designer',
+        email:'ariene.fox@example.com',
+        status:'Full Time',
+        salary:58000
+    }
+]
+
+export const columns:ColumnsType<DataType> = [
+    {
+        title:'Employee',
+        dataIndex:'employee',
+        key:'employee'
+    },
+    {
+        title:'Position',
+        dataIndex:'position',
+        key:'position'
+    },
+    {
+        title:'Email',
+        dataIndex:'email',
+        key:'email'
+    },
+    {
+        title:'Status',
+        dataIndex:'status',
+        key:'status'
+    },
+    {
+        title:'Salary/Year $',
+        dataIndex:'salary',
+        key:'salary'
+    },
+]
+
