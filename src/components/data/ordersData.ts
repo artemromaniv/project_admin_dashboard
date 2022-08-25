cec2240d94dbe6d9901b9fab00d3b348f0b771f6
artem_romaniv <artem_romaniv@protonmail.com>
2022-08-24 18:43:13 -0700

    fixes to css nad updates to orders table

--- src/components/data/ordersData.ts
+++ src/components/data/ordersData.ts
@@ -1,7 +1,7 @@
 import type {ColumnsType} from 'antd/es/table'
 import dayjs from 'dayjs'
 interface DataType {
-    key:number,
+    key:string
     orderID:number,
     customer:string,
     order:string[],
@@ -13,14 +13,168 @@ interface DataType {
 
 export const data: DataType[] = [
     {
-        key:747498,
+        key:'1',
         orderID:747498,
         order:['boat'],
         deliveryDate:dayjs('2022-05-25').format('DD.MM.YYYY'),
-        customer:'dude',
-        deliveryPricing:`${93} $`,
-        deliveryStatus:'continue'
-    }
+        customer:'Leon Shelton',
+        deliveryPricing:`${1099} $`,
+        deliveryStatus:'Continue'
+    },
+    {
+        key:'2',
+        orderID:83923080,
+        order:['boat'],
+        deliveryDate:`${dayjs('2022-05-25').format('DD.MM.YYYY')} (Estimated)`,
+        customer:'Genesis Martinez',
+        deliveryPricing:`${1799} $`,
+        deliveryStatus:'Continue'
+    },
+    {
+        key:'3',
+        orderID:4049830,
+        order:['boat', 'boats', 'boot'],
+        deliveryDate:dayjs('2022-08-25').format('DD.MM.YYYY'),
+        customer:'Takuma Watanabe',
+        deliveryPricing:`${2255.89} $`,
+        deliveryStatus:'Delievered'
+    },
+    {
+        key:'4',
+        orderID:336287369,
+        order:['boat', 'boats', 'boot'],
+        deliveryDate:dayjs('2022-07-13').format('DD.MM.YYYY'),
+        customer:'Leon Shelton',
+        deliveryPricing:`${699} $`,
+        deliveryStatus:'Continue'
+    },
+    {
+        key:'5',
+        orderID:463964929,
+        order:['boat', 'boats', 'boot'],
+        deliveryDate:'-',
+        customer:'Adrian Hill',
+        deliveryPricing:`${99} $`,
+        deliveryStatus:'Canceled'
+    },
+    {
+        key:'6',
+        orderID:574574073,
+        order:['boat', 'boats', 'boot'],
+        deliveryDate:'-',
+        customer:'Adrian Hill',
+        deliveryPricing:`${99} $`,
+        deliveryStatus:'Canceled'
+    },
+    {
+        key:'7',
+        orderID:87969003,
+        order:['boat', 'boats', 'boot'],
+        deliveryDate:'-',
+        customer:'Adrian Hill',
+        deliveryPricing:`${99} $`,
+        deliveryStatus:'Canceled'
+    },
+    {
+        key:'8',
+        orderID:383833328,
+        order:['boat', 'boats', 'boot'],
+        deliveryDate:'-',
+        customer:'Adrian Hill',
+        deliveryPricing:`${99} $`,
+        deliveryStatus:'Canceled'
+    },
+    {
+        key:'9',
+        orderID:92202787629,
+        order:['boat', 'boats', 'boot'],
+        deliveryDate:'-',
+        customer:'Adrian Hill',
+        deliveryPricing:`${99} $`,
+        deliveryStatus:'Canceled'
+    },
+    {
+        key:'10',
+        orderID:2773907320,
+        order:['boat', 'boats', 'boot'],
+        deliveryDate:'-',
+        customer:'Adrian Hill',
+        deliveryPricing:`${99} $`,
+        deliveryStatus:'Canceled'
+    },
+    {
+        key:'11',
+        orderID:380923720372,
+        order:['boat'],
+        deliveryDate:dayjs('2022-05-25').format('DD.MM.YYYY'),
+        customer:'Leon Shelton',
+        deliveryPricing:`${1099} $`,
+        deliveryStatus:'Continue'
+    },
+    {
+        key:'12',
+        orderID:3837023720397,
+        order:['boat'],
+        deliveryDate:`${dayjs('2022-05-25').format('DD.MM.YYYY')} (Estimated)`,
+        customer:'Genesis Martinez',
+        deliveryPricing:`${1799} $`,
+        deliveryStatus:'Continue'
+    },
+    {
+        key:'13',
+        orderID:4049830,
+        order:['boat', 'boats', 'boot'],
+        deliveryDate:dayjs('2022-08-25').format('DD.MM.YYYY'),
+        customer:'Takuma Watanabe',
+        deliveryPricing:`${2255.89} $`,
+        deliveryStatus:'Delievered'
+    },
+    {
+        key:'14',
+        orderID:336287369,
+        order:['boat', 'boats', 'boot'],
+        deliveryDate:dayjs('2022-07-13').format('DD.MM.YYYY'),
+        customer:'Leon Shelton',
+        deliveryPricing:`${699} $`,
+        deliveryStatus:'Continue'
+    },
+    {
+        key:'15',
+        orderID:463964929,
+        order:['boat', 'boats', 'boot'],
+        deliveryDate:'-',
+        customer:'Adrian Hill',
+        deliveryPricing:`${99} $`,
+        deliveryStatus:'Canceled'
+    },
+    {
+        key:'16',
+        orderID:463964929,
+        order:['boat', 'boats', 'boot'],
+        deliveryDate:'-',
+        customer:'Adrian Hill',
+        deliveryPricing:`${99} $`,
+        deliveryStatus:'Canceled'
+    },
+    {
+        key:'17',
+        orderID:463964929,
+        order:['boat', 'boats', 'boot'],
+        deliveryDate:'-',
+        customer:'Adrian Hill',
+        deliveryPricing:`${99} $`,
+        deliveryStatus:'Canceled'
+    },
+    {
+        key:'18',
+        orderID:463964929,
+        order:['boat', 'boats', 'boot'],
+        deliveryDate:'-',
+        customer:'Adrian Hill',
+        deliveryPricing:`${99} $`,
+        deliveryStatus:'Canceled'
+    },
+
 ]
 
 export const columns:ColumnsType<DataType> = [
@@ -45,7 +199,7 @@ export const columns:ColumnsType<DataType> = [
         key:'customer'
     },
     {
-        title:'Delivery Pricing $',
+        title:'Delivery Pricing',
         dataIndex:'deliveryPricing',
         key:'deliveryPricing'
     },
