1bbfac43bd471b004bc2f0fea5682bbacffc9717
Artem <artem_romaniv@protonmail.com>
2022-08-26 03:21:02 -0700

    ad ui features to orders table and predefine content of BestSellers Page

--- src/components/data/ordersData.tsx
+++ src/components/data/ordersData.tsx
@@ -1,5 +1,6 @@
 import type {ColumnsType} from 'antd/es/table'
 import dayjs from 'dayjs'
+import {Tag} from 'antd'
 interface DataType {
     key:string
     orderID:number,
@@ -14,44 +15,44 @@ interface DataType {
 export const data: DataType[] = [
     {
         key:'1',
-        orderID:747498,
-        order:['boat'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['Sony Playstation 5'],
         deliveryDate:`${dayjs('2022-09-01').format('DD.MM.YYYY')} (Estimated)`,
         customer:'Leon Shelton',
-        deliveryPricing:`${1099} $`,
+        deliveryPricing:`${699} $`,
         deliveryStatus:'Continue'
     },
     {
         key:'2',
-        orderID:83923080,
-        order:['boat'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['Apple Air Pods 2nd Gen','Apple Watch Series 3'],
         deliveryDate:`${dayjs('2022-09-26').format('DD.MM.YYYY')} (Estimated)`,
         customer:'Genesis Martinez',
-        deliveryPricing:`${1799} $`,
+        deliveryPricing:`${799} $`,
         deliveryStatus:'Continue'
     },
     {
         key:'3',
-        orderID:4049830,
-        order:['boat', 'boats', 'boot'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['Huawei Matebook 14s (Linux Preinstalled'],
         deliveryDate:dayjs('2022-06-15').format('DD.MM.YYYY'),
         customer:'Takuma Watanabe',
-        deliveryPricing:`${2255.89} $`,
+        deliveryPricing:`${2199} $`,
         deliveryStatus:'Delievered'
     },
     {
         key:'4',
-        orderID:336287369,
-        order:['boat', 'boats', 'boot'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['Seagate Portable 2TB External Hard Drive'],
         deliveryDate:`${dayjs('2022-11-04').format('DD.MM.YYYY')} (Estimated)`,
         customer:'Leon Shelton',
-        deliveryPricing:`${26499.89} $`,
+        deliveryPricing:`${499} $`,
         deliveryStatus:'Continue'
     },
     {
         key:'5',
-        orderID:463964929,
-        order:['boat', 'boats', 'boot'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['Apple Pencil (2nd Generation), White'],
         deliveryDate:`${dayjs('2022-08-27').format('DD.MM.YYYY')} (Estimated)`,
         customer:'Adrian Hill',
         deliveryPricing:`${99} $`,
@@ -59,17 +60,17 @@ export const data: DataType[] = [
     },
     {
         key:'6',
-        orderID:574574073,
-        order:['boat', 'boats', 'boot'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['Razer Basilisk V3'],
         deliveryDate:`${dayjs('2022-10-03').format('DD.MM.YYYY')} (Estimated)`,
         customer:'Frederick Caldwell',
-        deliveryPricing:`${5399} $`,
+        deliveryPricing:`${75} $`,
         deliveryStatus:'Continue'
     },
     {
         key:'7',
-        orderID:87969003,
-        order:['boat', 'boats', 'boot'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['Apple Pencil (1st Generation), White'],
         deliveryDate:'-',
         customer:'Justin Fowler',
         deliveryPricing:`${19.99} $`,
@@ -77,101 +78,101 @@ export const data: DataType[] = [
     },
     {
         key:'8',
-        orderID:383833328,
-        order:['boat', 'boats', 'boot'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['Sony Playstation 5'],
         deliveryDate:dayjs('2022-08-03').format('DD.MM.YYYY'),
         customer:'Wade Owens',
-        deliveryPricing:`${8099} $`,
+        deliveryPricing:`${699} $`,
         deliveryStatus:'Delivered'
     },
     {
         key:'9',
-        orderID:92202787629,
-        order:['boat', 'boats', 'boot'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['Matein Travel Laptop Backpack'],
         deliveryDate:`${dayjs('2022-08-28').format('DD.MM.YYYY')} (Estimated)`,
         customer:'Tracey Silva',
-        deliveryPricing:`${299} $`,
+        deliveryPricing:`${199} $`,
         deliveryStatus:'Continue'
     },
     {
         key:'10',
-        orderID:2773907320,
-        order:['boat', 'boats', 'boot'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['Sony Playstation 5','Razer Kraken Gaming Headset, Pink'],
         deliveryDate:dayjs('2022-07-18').format('DD.MM.YYYY'),
         customer:'Willie Gray',
-        deliveryPricing:`${599} $`,
+        deliveryPricing:`${839} $`,
         deliveryStatus:'Delivered'
     },
     {
         key:'11',
-        orderID:380923720372,
-        order:['boat'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['AMD Radeon RX 6500 XT'],
         deliveryDate:`${dayjs('2022-09-01').format('DD.MM.YYYY')} (Estimated)`,
         customer:'Hamada Chiyo',
-        deliveryPricing:`${1099} $`,
+        deliveryPricing:`${1075} $`,
         deliveryStatus:'Continue'
     },
     {
         key:'12',
-        orderID:3837023720397,
-        order:['boat'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['Macbook Air Pro 2022'],
         deliveryDate:`${dayjs('2022-09-15').format('DD.MM.YYYY')} (Estimated)`,
         customer:'Nonaka Michi',
-        deliveryPricing:`${7799} $`,
+        deliveryPricing:`${3999} $`,
         deliveryStatus:'Continue'
     },
     {
         key:'13',
-        orderID:4049830,
-        order:['boat', 'boats', 'boot'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['Apple Air Pods 2nd Gen'],
         deliveryDate:dayjs('2022-06-12').format('DD.MM.YYYY'),
         customer:'Takuma Watanabe',
-        deliveryPricing:`${2255.89} $`,
+        deliveryPricing:`${299} $`,
         deliveryStatus:'Delievered'
     },
     {
         key:'14',
-        orderID:336287369,
-        order:['boat', 'boats', 'boot'],
-        deliveryDate:'-',
+        orderID:Math.floor(Math.random()*100000000),
+        order:['Microsoft Surface Pro 2'],
+        deliveryDate:'',
         customer:'Clara Mcdonalid',
-        deliveryPricing:`${399} $`,
+        deliveryPricing:`${1385} $`,
         deliveryStatus:'Canveled'
     },
     {
         key:'15',
-        orderID:463964929,
-        order:['boat', 'boats', 'boot'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['Logitech G PRO X'],
         deliveryDate:dayjs('2022-06-23').format('DD.MM.YYYY'),
         customer:'Nomura Shizuko',
-        deliveryPricing:`${399} $`,
+        deliveryPricing:`${55} $`,
         deliveryStatus:'Delivered'
     },
     {
         key:'16',
-        orderID:6089764,
-        order:['boat', 'boats', 'boot'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['Sony Playstation 5'],
         deliveryDate:dayjs('2022-06-11').format('DD.MM.YYYY'),
         customer:'Kaneko Yuu',
-        deliveryPricing:`${999} $`,
+        deliveryPricing:`${699} $`,
         deliveryStatus:'Delivered'
     },
     {
         key:'17',
-        orderID:33586524,
-        order:['boat', 'boats', 'boot'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['Huawei Matebook 14s (Linux Preinstalled)'],
         deliveryDate:dayjs('2022-04-10').format('DD.MM.YYYY'),
         customer:'Kylie Welch',
-        deliveryPricing:`${1199} $`,
+        deliveryPricing:`${2199} $`,
         deliveryStatus:'Delivered'
     },
     {
         key:'18',
-        orderID:146775467,
-        order:['boat', 'boats', 'boot'],
+        orderID:Math.floor(Math.random()*100000000),
+        order:['SAMSUNG Galaxy Tab A8 '],
         deliveryDate:dayjs('2022-02-09').format('DD.MM.YYYY'),
         customer:'Yokoyama Shou',
-        deliveryPricing:`${7899} $`,
+        deliveryPricing:`${215} $`,
         deliveryStatus:'Delivered'
     },
 
@@ -186,7 +187,18 @@ export const columns:ColumnsType<DataType> = [
     {
         title:'Order',
         dataIndex:'order',
-        key:'order'
+        key:'order',
+        render: (_, { order }) => (
+            <>
+              {order.map(orderData => {
+                return (
+                  <Tag color={'geekblue'} key={orderData}>
+                    {orderData.toUpperCase()}
+                  </Tag>
+                );
+              })}
+            </>
+          ),
     },
     {
         title:'Delivery Date',
@@ -206,6 +218,13 @@ export const columns:ColumnsType<DataType> = [
     {
         title:'Delivery Status',
         dataIndex:'deliveryStatus',
-        key:'deliveryStatus'
+        key:'deliveryStatus',
+        render: (_, { deliveryStatus }) => (
+            <>
+                <Tag color={''} key={deliveryStatus}>
+                    {deliveryStatus}
+                </Tag>
+            </>
+          ),
     },
 ]
