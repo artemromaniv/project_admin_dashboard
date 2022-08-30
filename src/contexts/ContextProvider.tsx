4e63e50403181f88e6f4ec35eacbc36fbff26210
Artem <artemromaniv@yahoo.com>
2022-08-30 11:26:01 -0700

    minor changes

--- src/contexts/ContextProvider.tsx
+++ src/contexts/ContextProvider.tsx
@@ -1,15 +1,17 @@
 
-import React,{ createContext, Dispatch, SetStateAction, useContext, useState } from "react";
+import React,{ createContext,SetStateAction, useContext, useState } from "react";
 
 interface StateContextType {
   collapsed:boolean,
-  setCollapsed:Dispatch<SetStateAction<boolean>>
+  setCollapsed:React.Dispatch<SetStateAction<boolean>>
 }
 
 const StateContext = createContext<StateContextType>(null!);
 
 export const ContextProvider = ({children}:React.PropsWithChildren) => {
+
   const [collapsed,setCollapsed] = useState(false)
+
   return (
     <StateContext.Provider value={{collapsed,setCollapsed}}>
         {children}
