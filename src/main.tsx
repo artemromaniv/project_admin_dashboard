ea5929425c9576e947f405bcaf78f71288338da4
Artem Romaniv <artemromaniv@yahoo.com>
2022-09-20 22:27:18 -0700

    add mantine provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import {ContextProvider} from './contexts/ContextProvider'
+import { MantineProvider } from '@mantine/core'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ContextProvider>
       <BrowserRouter>
-        <App />
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+          <App />
+        </MantineProvider>
       </BrowserRouter>
     </ContextProvider>
   </React.StrictMode>
