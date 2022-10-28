5f9b20e0ee3f1ed224234da0adcb762c41025fbf
Artem <artemromaniv@yahoo.com>
2022-10-28 18:53:56 +0000

    add nav links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,42 @@
 import { useStateContext } from "../contexts/ContextProvider";
+import { NavLink } from "react-router-dom";
+import { links } from "./data/links"
+
+const nav_links = links.map((item) => (
+  <div key={item.title}>
+    <span >{item.title}</span>
+    <div >
+      {item.content.map((link) => (
+        <NavLink 
+          to={`/${link.path}`} 
+          key = {link.name} 
+          className = {({isActive}) => (isActive ? '' : '')}
+        >
+          {link.icon}
+          <span className="link-title">{link.name}</span>
+        </NavLink>
+      ))}
+    </div>
+  </div>
+))
+
 const NavBar = () => {
+
   const { collapsed, setCollapsed } = useStateContext();
+
   const handleCollapse = () => {
     setCollapsed(!collapsed);
   };
+
   return (
-    <nav  className={`top-0 left-0 w-[35vw] bg-blue-600  p-10 pl-20 text-white fixed h-full z-40  ease-in-out duration-300 ${
-      collapsed ? "translate-x-0 " : "-translate-x-full"
-    }`}>
+    <nav
+      className={`top-0 left-0 w-[35vw] bg-blue-600  p-10 pl-20 text-white fixed h-full z-40  ease-in-out duration-300 ${
+        collapsed ? "-translate-x-full " : "translate-x-0"
+      }`}
+    >
+      {nav_links}
       <button onClick={handleCollapse}>X</button>
-      <span>{collapsed ? '1' : '0'}</span>
+      <span>{collapsed ? "1" : "0"}</span>
     </nav>
   );
 };
