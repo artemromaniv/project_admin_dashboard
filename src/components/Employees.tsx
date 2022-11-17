a2acedf6c8aa7e218e64b0cdaa048cfb88f026bb
Artem <artemromaniv@yahoo.com>
2022-11-17 15:16:40 +0000

    add employee table

--- src/components/Employees.tsx
+++ src/components/Employees.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { data } from "./data/employeeData";
 
 const Employees = () => {
   return (
-    <div>Employees</div>
-  )
-}
+    <section className="overflow-x-auto">
+      <table className="table w-full overflow-y-scroll">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Status</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* <tr>
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+      </tr> */}
+          {data.map((employee) => (
+            <tr key={employee.employee}>
+              <th>{employee.employee}</th>
+              <td>{employee.position}</td>
+              <td>{employee.status}</td>
+              <td>{employee.salary}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
+};
 
-export default Employees
+export default Employees;
