4733bd5f53bd06893e584f1b8bbf82a6c274d9ec
Artem <artemromaniv@yahoo.com>
2022-11-17 15:26:01 +0000

    add orders table

--- src/components/Orders.tsx
+++ src/components/Orders.tsx
@@ -1,28 +1,33 @@
-import { useState } from 'react';
-
 import {data} from './data/ordersData'
 
-
-
-
 const Orders = () => {
-  const [scrolled, setScrolled] = useState(false);
-
-  const rows = data.map((row) => (
-    <tr key={row.orderID}>
-      <td>{row.orderID}</td>
-      <td>{row.order}</td>
-      <td>{row.deliveryDate}</td>
-      <td>{row.customer}</td>
-      <td>{row.deliveryPricing}</td>
-      <td>{row.deliveryStatus}</td>
-    </tr>
-  ))
-
   return (
-    <div>
-
-    </div>
+    <section className="overflow-x-auto">
+      <table className="table w-full overflow-y-scroll">
+        <thead>
+          <tr>
+            <th>Order ID</th>
+            <th>Order</th>
+            <th>Customer</th>
+            <th>Price</th>
+            <th>Date of Shipping</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((order) => (
+            <tr key={order.key}>
+              <td>{order.orderID}</td>
+              <td>{order.order}</td>
+              <td>{order.customer}</td>
+              <td>{order.deliveryPricing}</td>
+              <td>{order.deliveryDate}</td>
+              <td>{order.deliveryStatus}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
   )
 }
 
