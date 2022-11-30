d26cfcaa72ef2df7f6d6ebfe7956c3496eef25e5
Artem <artemromaniv@yahoo.com>
2022-11-30 14:35:52 +0000

    change component design to more consistent and responsive

--- src/components/Customers.tsx
+++ src/components/Customers.tsx
@@ -1,27 +1,66 @@
 import { data } from "./data/customerData";
+
+{
+  /* <tr key={customer.key}>
+<td>{customer.name}</td>
+<td>{customer.email}</td>
+<td>{customer.location}</td>
+<td>{customer.lastActive}</td>
+</tr> */
+}
+
 const Customers = () => {
   return (
-    <section className="overflow-x-auto p-5">
-      <table className="table w-full overflow-y-scroll">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Location</th>
-            <th>Last Active</th>
-          </tr>
-        </thead>
-        <tbody >
-          {data.map((customer) => (
-            <tr key={customer.key}>
-              <td>{customer.name}</td>
-              <td>{customer.email}</td>
-              <td>{customer.location}</td>
-              <td>{customer.lastActive}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <section className="overflow-x-auto px-0 md:p-5 md:py-15 lg:px-24 xl:px-24">
+      <div className="p-3 w-full bg-white rounded-xl shadow-lg">
+        <div className="overflow-x-auto">
+          <table className="table-auto w-full">
+            <thead className="text-xs font-semibold uppercase text-gray-400 bg-gray-50 ">
+              <tr>
+                <th className="p-2 whitespace-nowrap">
+                  <div>Name</div>
+                </th>
+                <th className="p-2 whitespace-nowrap">
+                  <div>Email</div>
+                </th>
+                <th className="p-2 whitespace-nowrap">
+                  <div>Location</div>
+                </th>
+                <th className="p-2 whitespace-nowrap">
+                  <div>Last Active</div>
+                </th>
+              </tr>
+            </thead>
+
+            <tbody className="text-sm divide-y divide-gray-200">
+              {data.map((customer) => (
+                <tr key={customer.key}>
+                  <td className="p-4 whitespace-nowrap">
+                    <div className="font-bold text-gray-800">
+                      {customer.name}
+                    </div>
+                  </td>
+                  <td className="p-4 whitespace-nowrap">
+                    <div className="font-medium text-gray-800">
+                      {customer.email}
+                    </div>
+                  </td>
+                  <td className="p-4 whitespace-nowrap">
+                    <div className="font-meduim text-gray-800">
+                      {customer.location}
+                    </div>
+                  </td>
+                  <td className="p-4 whitespace-nowrap">
+                    <div className="font-meduim text-gray-800">
+                      {customer.lastActive}
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/Employees.tsx
+++ src/components/Employees.tsx
@@ -2,35 +2,64 @@ import { data } from "./data/employeeData";
 
 const Employees = () => {
   return (
-    <section className="overflow-x-auto p-5">
-      <table className="table w-full overflow-y-scroll">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Email</th>
-            <th>Status</th>
-            <th>Salary</th>
-          </tr>
-        </thead>
-        <tbody className="rounded-xl">
-          {/* <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr> */}
-          {data.map((employee) => (
-            <tr key={employee.employee}>
-              <td>{employee.employee}</td>
-              <td>{employee.email}</td>
-              <td>{employee.position}</td>
-              <td>{employee.status}</td>
-              <td>{employee.salary}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <section className="overflow-x-auto px-0 md:p-5 md:py-15 lg:px-24 xl:px-24">
+      <div className="p-3 w-full bg-white rounded-xl shadow-lg">
+        <div className="overflow-x-auto">
+          <table className="table-auto w-full">
+            <thead className="text-xs font-semibold uppercase text-gray-400 bg-gray-50 ">
+              <tr>
+                <th className="p-2 whitespace-nowrap">
+                  <div>Employee</div>
+                </th>
+                <th className="p-2 whitespace-nowrap">
+                  <div>Position</div>
+                </th>
+                <th className="p-2 whitespace-nowrap">
+                  <div>Email</div>
+                </th>
+                <th className="p-2 whitespace-nowrap">
+                  <div>Status</div>
+                </th>
+                <th className="p-2 whitespace-nowrap">
+                  <div>Salary</div>
+                </th>
+              </tr>
+            </thead>
+
+            <tbody className="text-sm divide-y divide-gray-200">
+              {data.map((employee) => (
+                <tr key={employee.employee}>
+                  <td className="p-4 whitespace-nowrap">
+                    <div className="font-bold text-gray-800">
+                      {employee.employee}
+                    </div>
+                  </td>
+                  <td className="p-4 whitespace-nowrap">
+                    <div className="font-medium text-gray-800">
+                      {employee.position}
+                    </div>
+                  </td>
+                  <td className="p-4 whitespace-nowrap">
+                    <div className="font-meduim text-gray-800">
+                      {employee.email}
+                    </div>
+                  </td>
+                  <td className="p-4 whitespace-nowrap">
+                    <div className="font-meduim text-gray-800">
+                      {employee.status}
+                    </div>
+                  </td>
+                  <td className="p-4 whitespace-nowrap">
+                    <div className="font-meduim text-gray-800">
+                      {employee.salary}
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </section>
   );
 };
