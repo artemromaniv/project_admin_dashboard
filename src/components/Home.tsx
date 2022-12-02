cf5abc0af0fedc373edbcfa67acfb733cd7cbbea
Artem <artemromaniv@yahoo.com>
2022-12-02 12:21:44 +0000

    add stats to homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,6 +8,7 @@ import {
   ArrowDownRight,
   Icon,
 } from "tabler-icons-react";
+import RevenueChart from "./charts/RevenueChart";
 
 const getRandomInt = (min: number, max: number) => {
   min = Math.ceil(min);
@@ -15,8 +16,8 @@ const getRandomInt = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const sales = getRandomInt(64000, 134000);
-const expenses = getRandomInt(20000, 45000);
+const sales = getRandomInt(640000, 1340000);
+const expenses = getRandomInt(120000, 345000);
 const income = sales - expenses;
 const newClients = getRandomInt(5000, 15000);
 
@@ -64,15 +65,40 @@ const Home = () => {
         />
       </div>
       <div className="h-8" />
-      <div className="grid grid-cols-4 gap-5">
-        <div className="bg-crust h-20 w-full rounded-2xl shadow-lg shadow-slate-200 col-span-3"></div>
-        <div className="bg-crust  w-full rounded-2xl shadow-lg shadow-slate-200 col-span-1"></div>
+      <div className="grid grid-cols-1  xl:grid-cols-4 gap-5 w-full">
+        <div className="bg-crust  w-full rounded-2xl shadow-lg shadow-slate-200 xl:col-span-3 p-5">
+          <RevenueChart/>
+        </div>
+        <div className="bg-crust  w-full rounded-2xl shadow-lg shadow-slate-200 col-span-1 grid place-items-center p-5 py-10">
+          {/* Typescript gives an error on inline style below, but it seems to work fine */}
+          <div
+            className="radial-progress font-bold text-3xl text-little_boy_blue"
+            style={{
+              // @ts-ignore
+              "--value": "70",
+              "--size": "12rem",
+              "--thickness": "26px",
+            }}
+          >
+            70%
+          </div>
+        </div>
       </div>
       <div className="h-8" />
-      <div className="grid grid-cols-4 gap-5">
-        <div className="bg-crust h-20 w-full rounded-2xl shadow-lg shadow-slate-200 col-span-2"></div>
-        <div className="bg-crust  w-full rounded-2xl shadow-lg shadow-slate-200 col-span-2"></div>
-      </div>
+      {/* <div className="grid grid-cols-1 xl:grid-cols-4 gap-5">
+        <div className="bg-crust  w-full rounded-2xl shadow-lg shadow-slate-200 col-span-2 p-5">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident,
+          laborum recusandae aliquam, unde eos dolorum perferendis quasi alias
+          vitae, explicabo voluptatum quam quidem dignissimos molestias pariatur
+          doloribus consectetur totam. Rerum!
+        </div>
+        <div className="bg-crust  w-full rounded-2xl shadow-lg shadow-slate-200 col-span-2 p-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+          nobis facere, reprehenderit temporibus officia iste? Error laborum
+          officia fugit nesciunt ut similique. Eum repudiandae deleniti
+          laboriosam. Ducimus voluptatibus a minima.
+        </div>
+      </div> */}
     </section>
   );
 };
